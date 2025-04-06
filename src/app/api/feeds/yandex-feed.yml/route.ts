f6ef1fcdcd4f648c77c4ea7d7cce6244a53de802
maxim <borisov130490@gmail.com>
2025-04-06 17:14:13 +0300

    fix:feed

--- src/app/api/feeds/yandex-feed.yml/route.ts
+++ src/app/api/feeds/yandex-feed.yml/route.ts
@@ -24,56 +24,55 @@ export async function GET() {
         <offers>
         ${data.data
           .map(
-            (product) =>
-              `<offer type="vendor.model" available="${
-                product.available
-              }" id="${product.slug}">
-            <url>${BASE_URL}/catalog/product/${product.slug}</url>
-            <price>${product.price}.00</price>
-            <currencyId>RUR</currencyId>
-            <picture>${process.env.NEXT_PUBLIC_URL}${
-                product?.images ? product?.images[0]?.url : "/image/kond.jpg"
-              }</picture>
-            <typePrefix>${product.category?.name}</typePrefix>
-            <vendor>${product.brand?.name}</vendor>
-            <model>${product.name}</model>
-            <sales_notes>Можем установить сегодня</sales_notes>
-            <manufacturer_warranty>true</manufacturer_warranty>
-            <vendorCode>${product.id}</vendorCode>
-            ${product.hit ? "<label>Хит</label>" : ""}
-            <param name="popularity">${product.popularity}</param>
-            <param name="Серия:">${product.series}</param>
-            <param name="Площадь помещения до м²:">${product.available}</param>
-            <param name="Технология компрессора:">${
-              product.compressor_type
-            }</param>
-            <param name="Наличие Wi-Fi:">${product.wifi_availability}</param>
-            <param name="Охлаждающая способность, BTU:">${
-              product.cooling_capacity
-            }</param>
-            <param name="Уровень шума, дБ:">${product.noise_level}</param>
-            <param name="Цвет:">${product.color}</param>
-            <param name="Класс энергоэффективности:">${
-              product.energy_efficiency_class
-            }</param>
-            <param name="Страна производитель:">${
-              product.country_of_manufacturer
-            }</param>
-            <param name="Гарантия:">${product.warranty_period}</param>
-
-            ${
-              product.char &&
-              product.char
-                .map(
-                  (description) =>
-                    `<param name="${description.key}">${description.value}</param>`
-                )
-                .join("")
-            }
+            (p) =>
+              `
+            <offer id="${p.id}" type="vendor.model" available="${p.available}">
+              <name>${p.brand?.name + " " + p.name}</name>
+              <categoryId>123</categoryId>
+              <url>${BASE_URL}/catalog/product/${p.slug}</url>
+              ${p.images?.map(
+                (i) =>
+                  `<picture>${process.env.NEXT_PUBLIC_URL}${i.url}</picture>`
+              )}
+              <price>${p.price}</price>
+              <oldprice>${
+                p.sale
+                  ? (p.price || 0) * (p.sale / 100) + (p.price || 0)
+                  : p.sale
+              }</oldprice>
+              <currencyId>RUR</currencyId>
+              <typePrefix>${p.category?.name}</typePrefix>
+              <model>${p.series}</model>
+              <vendor>${p.brand?.name}</vendor>
+              <description>${p.description}</description>
+              <sales_notes>Скидка 10% на первую покупку. Привезем и установим сегодня.</sales_notes>
+              <param name="Площадь помещения до м²:">${p.available}</param>
+              <param name="Технология компрессора:">${p.compressor_type}</param>
+              <param name="Наличие Wi-Fi:">${p.wifi_availability}</param>
+              <param name="Охлаждающая способность, BTU:">${
+                p.cooling_capacity
+              }</param>
+              <param name="Уровень шума, дБ:">${p.noise_level}</param>
+              <param name="Цвет:">${p.color}</param>
+              <param name="Класс энергоэффективности:">${
+                p.energy_efficiency_class
+              }</param>
+              <param name="Страна производитель:">${
+                p.country_of_manufacturer
+              }</param>
+              <param name="Гарантия:">${p.warranty_period}</param>
+              ${
+                p.char &&
+                p.char
+                  .map(
+                    (description) =>
+                      `<param name="${description.key}">${description.value}</param>`
+                  )
+                  .join("")
+              }
           </offer>`
           )
           .join("")}
-          
         </offers>
     </shop>
   </yml_catalog>
